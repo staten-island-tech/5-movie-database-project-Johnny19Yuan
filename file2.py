e12b20e4d938a92397dcd7335d45383c91f76f3d
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-30 09:14:14 -0400

    Update file2.py

--- file2.py
+++ file2.py
@@ -2,5 +2,4 @@ import json
 movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
 year = int(input("Search movies after the year: "))
-x = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-print(x)
+x = print(data[year]["title"])
