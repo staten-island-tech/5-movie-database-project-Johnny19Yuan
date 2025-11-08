85f222ab48074cdaa3fbfbb81d470c06c01a7713
Johnny19Yuan <johnny19yuan@gmail.com>
2025-11-08 17:07:05 -0500

    Update file2.py

--- file2.py
+++ file2.py
@@ -3,7 +3,10 @@ movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
 year = int(input("Search movies after the year: "))
 years=[]
+year =-1
+x = 0
 for index, item in enumerate(data):
-    year =-1
-    if 
+    years.append(item["year"])
+    if years[x] < year:
+        years.pop
 print(data[year]["title"])
