ec988acc12392de2b36daa28ff0fa974963395b7
Johnny19Yuan <johnny19yuan@gmail.com>
2025-11-13 09:25:00 -0500

    Update file5.py

--- file5.py
+++ file5.py
@@ -2,7 +2,8 @@ import json
 movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
 title = (input("Search for movie title: "))
+lowert = title.lower()
 for index, item in enumerate(data):
-        if (item["title"]) == title:
+        if (item["title"]).lower() == lowert:
             print(item["title"])
-#bro fix it it no work💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀67
+#bro fix it it no work 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
