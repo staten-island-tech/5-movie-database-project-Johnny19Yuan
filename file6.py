ec87cf102f53b5ccecae680fdf96f0e7134f04ee
Johnny19Yuan <johnny19yuan@gmail.com>
2025-11-17 09:25:22 -0500

    why.pyw

--- file6.py
+++ file6.py
@@ -1,5 +1,9 @@
 import json
 movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
-for index, item in enumerate(data):
-    print(item["genres"])
+genre = (input("Search for movie genre: "))
+
+for movie in data:
+    for item in movie["genres"]:
+        if item == genre:
+            print(movie["title"])
